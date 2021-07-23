8e3278a7ac66dcbb24fbc9cd013a0d541faab323
DraganSub <67284999+DraganSub@users.noreply.github.com>
2021-07-23 15:15:46 +0200

    Update firebase.js

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -2,14 +2,13 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDI-PwWLJGvsbH3WrSVCLQiP0cEOBi2Nco",
-  authDomain: "school-directory-efb9c.firebaseapp.com",
-  databaseURL: "https://school-directory-efb9c-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "school-directory-efb9c",
-  storageBucket: "school-directory-efb9c.appspot.com",
-  messagingSenderId: "887392865873",
-  appId: "1:887392865873:web:2ad27d5a6f5f9095f93270",
-  measurementId: "G-FCTHYMD0KC"
+  apiKey: "AIzaSyCZI7sNohyeljczFImotDDI4CnzBMKr9IE",
+  authDomain: "projekt-f9d15.firebaseapp.com",
+  projectId: "projekt-f9d15",
+  storageBucket: "projekt-f9d15.appspot.com",
+  messagingSenderId: "113314607257",
+  appId: "1:113314607257:web:8beffa290cdaa9eee6c808",
+  measurementId: "G-8Q6HZTSHX6"
 }
 
 firebase.initializeApp(firebaseConfig)
